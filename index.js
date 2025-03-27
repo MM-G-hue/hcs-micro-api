880de9e09afefd5973f9dcdc74c35b6d4b3292b8
MM-G-hue <marijn.martin@hotmail.com>
2025-03-27 13:55:46 +0100

    open fastify ip address accept

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ fastify.post('/message', async (request, reply) => {
 // Start the server
 const start = async () => {
     try {
-        await fastify.listen({ port: 3001 });
+        await fastify.listen({ port: 3001, host: '0.0.0.0' });
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
